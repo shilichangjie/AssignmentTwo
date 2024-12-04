9bbe91852857a69249ec3db4c9975b9572f7c854
shilichangjie <162394672+shilichangjie@users.noreply.github.com>
2024-12-04 17:47:24 +0800

    part3

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -2,30 +2,28 @@ package src;
 
 public class AssignmentTwo {
     public static void main(String[] args) {
-        
+        AssignmentTwo assignment = new AssignmentTwo();
+        assignment.partThree();
+
     }
 
     public void partThree() {
-       
+        // 1. 创建一个 `Ride` 对象。
+
+        Ride ride = new Ride("roller coaster", new Employee("Ivy Davis", 35, "Male", 2, "Operator"));
+        // 2. 至少向队列中添加 5 位游客。
+
+        ride.addVisitorToQueue(new Visitor("Alice Johnson", 30, "Female", "123-456-7890", "Tourist"));
+        ride.addVisitorToQueue(new Visitor("Bob Smith", 45, "Male", "234-567-8901", "VIP"));
+        ride.addVisitorToQueue(new Visitor("Charlie Brown", 28, "Male", "345-678-9012", "Regular"));
+        ride.addVisitorToQueue(new Visitor("David Williams", 35, "Male", "456-789-0123", "Tourist"));
+        ride.addVisitorToQueue(new Visitor("Eva Clark", 22, "Female", "567-890-1234", "VIP"));
+        // 3. 移除队列中的 1 位游客。
+
+        ride.removeVisitorFromQueue("Eva Clark");
+        // 4. 打印队列中所有游客。
+
+        ride.printQueue();
     }
 
-    public void partFourA() {
-
-    }
-
-    public void partFourB() {
-        
-    }
-
-    public void partFive() {
-        
-    }
-
-    public void partSix() {
-
-    }
-
-    public void partSeven() {
-       
-    }
 }
--- src/Ride.java
+++ src/Ride.java
@@ -1,16 +1,61 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Ride implements RideInterface {
     String rideName;
-    int maxRiders = 5;
     Employee operator;
+    ArrayList<Visitor> queue = new ArrayList<>();
+    LinkedList<Visitor> history = new LinkedList<>();
 
     public Ride() {
     }
 
-    public Ride(String rideName, int maxRiders, Employee operator) {
+    public Ride(String rideName, Employee operator) {
         this.rideName = rideName;
-        this.maxRiders = maxRiders;
         this.operator = operator;
     }
+
+    // 添加游客
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        queue.add(visitor);
+        System.out.println(" addVisitorToQueue success.");
+    }
+
+    // 删除游客
+    @Override
+    public void removeVisitorFromQueue(String visitorName) {
+        for (Visitor visitor : queue) {
+            if (visitor.getName().equals(visitorName)) {
+                queue.remove(visitor);
+                System.out.println("removeVisitorFromQueue success");
+                return;
+            }
+        }
+        System.out.println("removeVisitorFromQueue error");
+    }
+
+    // 打印游客列表
+    @Override
+    public void printQueue() {
+        for (Visitor visitor : queue) {
+            System.out.println(visitor.toString());
+        }
+    }
+
+
+
+
+
+    
+
 }
